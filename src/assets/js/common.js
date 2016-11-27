d0cd18feeaaf980db380309894214afbeb8ba867
jinqiangshou <271021733@qq.com>
2016-11-27 22:30:45 +0800

    add responsive effect

--- src/assets/js/common.js
+++ src/assets/js/common.js
@@ -3,10 +3,15 @@
         resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
         recalc = function () {
             var clientWidth = docEl.clientWidth;
+            var clientHeight = docEl.clientHeight;
             if (!clientWidth) return;
+            if (clientHeight < clientWidth) {
+                clientWidth = Math.min(clientWidth, 600);
+            }
             docEl.style.fontSize = 20 * (clientWidth / 320) + 'px';
         };
     if (!doc.addEventListener) return;
     win.addEventListener(resizeEvt, recalc, false);
     doc.addEventListener('DOMContentLoaded', recalc, false);
+    recalc();
 })(document, window);
